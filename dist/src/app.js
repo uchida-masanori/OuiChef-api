057bbde7353547df46f24387099df8690ddf5d19
uchida-masanori <masa11soc@icloud.com>
2024-01-26 16:55:44 +0900

    TSの環境調整

---
+++ dist/src/app.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const fastify = (0, fastify_1.default)({
+    logger: true
+});
+// Declare a route
+fastify.get('/', async function handler() {
+    return { hello: 'world' };
+});
+// Run the server!
+async function main() {
+    try {
+        await fastify.listen({ port: 3000 });
+    }
+    catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+main();
--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,13 @@ fastify.get('/', async function handler () {
 })
 
 // Run the server!
+async function main(){
 try {
   await fastify.listen({ port: 3000 })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
 }
+}
+
+main();
